f82e893ead6eeb859ba13d6ac93208e1cbb030c7
Lorenzo Destriere <lorenzo.destriere@gmail.com>
2025-09-25 15:11:02 +0200

    feat: aggiunge ricerca per nome del ristorante

---
+++ src/main/java/systems/lordes/server/utils/RestaurantSpecifications.java
@@ -0,0 +1,23 @@
+package systems.lordes.server.utils;
+
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
+import org.springframework.data.jpa.domain.Specification;
+import systems.lordes.server.entity.RestaurantEntity;
+
+public class RestaurantSpecifications {
+
+    public static Specification<RestaurantEntity> searchByKeyword(String search) {
+        return (Root<RestaurantEntity> root, CriteriaQuery<?> query, CriteriaBuilder cb) -> {
+            if (search == null || search.isBlank()) {
+                return cb.conjunction();
+            }
+
+            String likeSearch = "%" + search.toLowerCase() + "%";
+            return cb.or(
+                cb.like(cb.lower(root.get("name")), likeSearch)
+            );
+        };
+    }
+}
